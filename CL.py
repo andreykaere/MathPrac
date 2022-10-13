f42e2a1d9585d6562ce041064a5b497437ec2867
Andrey <andreykaere@gmail.com>
2022-10-13 23:04:27 +0300

    enhanced version of algorithm

--- CL.py
+++ CL.py
@@ -1,47 +1,50 @@
+#!/usr/bin/python3
+
 import time
-start_time = time.time()
+from math import sqrt
+
 
 def is_square(x):
-    # ? correct range?
-    for i in range(x):
-        if (i * i) == x:
+    for i in range(sqrt(x) + 1):
+        if i**2 == x:
             return True
     return False
+
 
 def is_square_mod(x, a):
-    # ? check?
-    if a == 0:
-        return True
-    # ? correct range?
-    for i in range(1, abs(a) + 2):
-        if ((i * i) - x) % a == 0:
+    for i in range(a):
+        if (i**2 - x) % a == 0:
             return True
     return False
-
 
 
 def CL(a, b, c):
-    return is_square_mod(b*c, a) and is_square_mod(a*c, b) and is_square_mod(-a*b, c)
+    return is_square_mod(b * c, a) and \
+           is_square_mod(a * c, b) and \
+           is_square_mod(-a * b, c)
+
 
 def find_solution(a, b, c):
+    x_range = int((b * c) ** (1/2)) + 1
+    y_range = int((a * c) ** (1/2)) + 1
+    z_range = int((a * b) ** (1/2)) + 1
 
-    X = int((b * c) ** (1/2)) + 1
-    Y = int((a * c) ** (1/2)) + 1
-    Z = int((a * b) ** (1/2)) + 1
     # mb not enough precision
-    for x0 in range(X):
-        for y0 in range(Y):
-            # ? mb improve
-            for z0 in range(Z):
-                if ((a*x0 * x0 + b*y0*y0 - c * z0*z0) == 0) and ((x0 != 0) or (y0 != 0) or (z0 != 0)):
-                    return (x0, y0, z0)
+    for x0 in range(x_range):
+        for y0 in range(y_range):
+            z0 = (a * x0**2 + b * y0**2)/c
+
+            if is_square(z0) and (x0 != 0 or y0 != 0 or z0 != 0):
+                return (x0, y0, z0)
     
 
 def main():
     print("input a, b, c")
+    
     a = int(input())
     b = int(input())
     c = int(input())
+
     start_time = time.time()
     if (not CL(a, b, c)):
         print("No solution")
@@ -54,16 +57,6 @@ def main():
     
     print("-------- %s seconds -------" % (time.time() - start_time))
     print()
-        
-
-while True:
-    main()
 
 
-
-
-
-
-
-
-
+main()
